8a7f1692e4ed02b83eda4196c1d9a5cd6089f11d
IgorP <wadwad123@mail.ru>
2017-04-26 15:49:05 +0300

    created home page

--- app/contacts/contacts.component.ts
+++ app/contacts/contacts.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cv-contacts',
-  templateUrl: './contacts.component.html'
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent {
-
+  title = 'Conatact Me';
 }
--- app/resume/resume.component.ts
+++ app/resume/resume.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'cv-resume',
   templateUrl: './resume.component.html',
-  styles: [`
-    img[alt="photo"]{
-      width: auto;
-      max-height: 250px;
-      border-radius: 150px;
-    }
-  `]
+  styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
   person = {
     name: 'Andrii Dovbenko',
+    dateOfBirth: '14 December 1990;',
     logoSrc: '../../assets/logo.jpg',
     location: 'Kalush, Ukraine',
     tel: '+380503732085',
     email: 'andrijdovbenko@gmail.com',
     skype: 'dar141290',
+    github: 'https://github.com/andrijdovbenko',
+    skils: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap'],
+    languages: ['Ukrainian', 'English'],
+    certifications: [
+      'Date of studying: May 16 – June 13, 2016; HTML5 Applications Development Fundamentals (HTML5/CSS3/JavaScript). SoftServe University.',
+      'Date of studying: September 5 – October 17, 2016; Basics of Web UI development. Lviv IT School, Prometheus.'
+    ],
+    educations: [
+      'Date of studying: 2011-2014; name of institution: Lviv Polytechnic National University; qualification: specialist’s degree in Enterprise economics.',
+      'Date of studying: 2011-2012; name of institution: Lviv Polytechnic National University; qualification: specialist’s degree in Heating engineering.',
+      'Date of studying: 2007-2011; name of institution: Lviv Polytechnic National University; qualification: bachelor’s degree in Heating engineering.'
+    ]
+  }
 
-  };
+
+
 
 }
+
