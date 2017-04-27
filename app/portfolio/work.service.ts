7ea80063f7f238c92e6381bd80f4e8258fda433f
Andrii Dovbenko <andrijdovbenko@gmail.com>
2017-04-27 22:38:31 +0300

    work details v3

--- app/portfolio/work.service.ts
+++ app/portfolio/work.service.ts
@@ -5,9 +5,9 @@ import { Work } from './work';
 export class WorkService {
 
   works: Work[] = [
-    new Work('eleoquii', 'https://github.com/andrijdovbenko/eleoquii', ['HTML', 'CSS', 'JavaScript', 'jQuery'], 'http://edu.cbsystematics.com/Content/Education/FrontEnd/images/angular.png'),
-    new Work('weatherforecast', 'https://github.com/andrijdovbenko/weather-forecast', ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'jQuery'], 'http://edu.cbsystematics.com/Content/Education/FrontEnd/images/angular.png'),
-    new Work('appz', 'https://github.com/andrijdovbenko/appz', ['HTML', 'CSS', 'Bootstrap'], 'http://edu.cbsystematics.com/Content/Education/FrontEnd/images/angular.png'),
+    new Work('eleoquii', 'https://github.com/andrijdovbenko/eleoquii', ['HTML', 'CSS', 'JavaScript', 'jQuery'], '../assets/eleoquii.jpg'),
+    new Work('weatherforecast', 'https://github.com/andrijdovbenko/weather-forecast', ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'jQuery'], '../assets/weatherforecast.jpg'),
+    new Work('appz', 'https://github.com/andrijdovbenko/appz', ['HTML', 'CSS', 'Bootstrap'], '../assets/appz.jpg'),
     new Work('resume', 'https://andrijdovbenko.github.io', ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'Angular 4'], '../assets/resume.jpg'),
     new Work('jetro', 'https://github.com/andrijdovbenko/jetro', ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'jQuery'], '../assets/jetro.jpg')
   ];
